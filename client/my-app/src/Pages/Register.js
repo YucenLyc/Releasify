9993a7242db28084ea323c07dd41eee9cd40cdf0
YucenLyc <yucen.u@gmail.com>
2021-09-15 14:44:26 -0700

    add register on the working front page/connect to backend registration route

---
+++ client/my-app/src/Pages/Register.js
@@ -0,0 +1,49 @@
+import React, { useState } from "react"
+import axios from "axios";
+
+function Registration() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const register = () => {
+    axios.post("http://localhost:3000/register", {
+      name: name,
+      email: email,
+      password: password,
+    }).then((response) => {
+      console.log(response);
+    });
+  }
+
+  return (
+    <div className="App">
+      <div className="registration">
+        <h1>Registration</h1>
+        <label>Username</label>
+        <input
+          type="text"
+          onChange={(event) => {
+            setName(event.target.value);
+          }}
+        />
+        <label>Email</label>
+        <input
+          type="text"
+          onChange={(event) => {
+            setEmail(event.target.value);
+          }}
+        />
+        <label>Password</label>
+        <input
+          type="text"
+          onChange={(event) => {
+            setPassword(event.target.value);
+          }}
+        />
+        <button onClick={register}>Register</button>
+      </div>
+    </div>
+  )
+};
+export default Registration;

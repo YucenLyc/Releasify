4444d4a315cf2f2e4e6e8bd3f7a35df8eff8fee8
YucenLyc <yucen.u@gmail.com>
2021-09-29 17:48:57 -0700

    frontend login connected to BE

--- client/my-app/src/Pages/Login.js
+++ client/my-app/src/Pages/Login.js
@@ -1,42 +1,137 @@
 import React, { useState } from 'react';
-import axios from "axios";
- 
+import axios from 'axios';
+
+
 function Login() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword]= useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const login = () => {
-    const data = { email: email, password: password };
-    axios
-      .post("http://localhost:3000/login", data)
-      .then((response) => {
-        if (response.data.error){
-          alert(response.data.error)
-        } else {
-        console.log("email:", response.data.email)
-        console.log("id", response.data.id)
-      }
-    })  
-  };
- 
+    axios.post("http://localhost:3000/api/auth/login", {
+      email: email,
+      password: password,
+    }).then((response) => {
+      console.log("user logged in");
+    });
+  }
+
+
   return (
-    <div>
-      Login<br /><br />
-      <div>
-        Email<br />
-        <input type="text" onChange={(event) =>{
+    <div className="login-wrapper">
+      <h1>Please Login</h1>
+      <label>
+        <p>Email</p>
+        <input type="text" onChange={(event) => {
           setEmail(event.target.value)
-        }} />
-      </div>
-      <div style={{ marginTop: 10 }}>
-        Password<br />
-        <input type="password" onChange={(event)=>{
+        }}
+        />
+      </label>
+      <label>
+        <p>Password</p>
+        <input type="password" onChange={(event) => {
           setPassword(event.target.value)
-        }} />
+        }}
+        />
+      </label>
+      <div>
+        <button onClick={login}>Login</button>
       </div>
-      <br />
-      <button onClick={login}>Login</button>
     </div>
-  );
-};
+  )
+}
+
 export default Login;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React, { useState } from 'react';
+// import axios from "axios";
+
+// function Login() {
+//   const [email, setEmail] = useState('');
+//   const [password, setPassword]= useState('');
+
+//   const login = () => {
+//     const data = { email: email, password: password };
+//     axios
+//       .post("http://localhost:3000/api/auth/login", data)
+//       .then((response) => {
+//         if (response.data.error){
+//           alert(response.data.error)
+//         } else {
+//         console.log("successfully logged in")
+//       }
+//     })  
+//   };
+
+//   return (
+//     <div>
+//       Login<br /><br />
+//       <div>
+//         Email<br />
+//         <input type="text" onChange={(event) =>{
+//           setEmail(event.target.value)
+//         }} />
+//       </div>
+//       <div style={{ marginTop: 10 }}>
+//         Password<br />
+//         <input type="password" onChange={(event)=>{
+//           setPassword(event.target.value)
+//         }} />
+//       </div>
+//       <br />
+//       <button onClick={login}>Login</button>
+//     </div>
+//   );
+// };
+// export default Login;
